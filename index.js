e889ab75c2e6b7f8976a1ef677a0594c4e0e86a5
Alex Njoroge <alex.njoroge@student.moringaschool.com>
2024-01-10 16:17:14 -0800

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    console.log['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(0,2)
+
+}
+function returnLastTwoDrivers(drivers){
+    console.log['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(-2);
+
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+    
+
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+
+    }
+}
+
+function fareDoubler(fare){
+    return fare * 2;
+
+}
+
+function fareTripler(fare){
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    if (selectingDrivers === returnFirstTwoDrivers) {
+        return drivers.slice(0,2);
+    }
+    else if (selectingDrivers === returnLastTwoDrivers) {
+        return drivers.slice(-2);
+        
+    }
+
+}
